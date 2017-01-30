ee5d418610fa4e3a3019677e302135e00ec89e94
Dale Seo <dale.seo@gmail.com>
2017-01-30 16:08:29 +0900

    web security test

--- src/main/java/readinglist/SecurityConfig.java
+++ src/main/java/readinglist/SecurityConfig.java
@@ -1,10 +1,13 @@
 package readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -26,6 +29,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> readerRepository.findOne(username));
+        auth.userDetailsService(userDetailsService());
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> readerRepository.findOne(username);
+    }
+
 }
---
+++ src/test/java/readinglist/MockMvcWebTests.java
@@ -0,0 +1,62 @@
+package readinglist;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void homePage_unauthenticatedUser() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "http://localhost/login"))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails("craig")
+    public void homePage_authenticatedUser() throws Exception {
+        Reader expectedReader = new Reader();
+        expectedReader.setUsername("craig");
+        expectedReader.setPassword("password");
+        expectedReader.setFullname("Craig Walls");
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attribute("reader", samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)))
+                .andExpect(model().attribute("amazonID", "dale0713"))
+                .andDo(print());
+    }
+}
